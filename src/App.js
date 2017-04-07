3cc390525848d69142d9b772daff6bb9d4232330
Alan Joseph <alanjsph@live.com>
2017-04-07 13:14:39 +0530

    basic working

--- src/App.js
+++ src/App.js
@@ -3,12 +3,31 @@ import Projects from './components/Projects';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      projects: [
+        {
+          title : "Business Website",
+          category : "Web design"
+        },
+        {
+          title : "Social App",
+          category : "Mobile development"
+        },
+        {
+          title : "E-commerce shopping cart",
+          category : "Web development"
+        }
+      ]
+    }
+  }
   render() {
     return (
       <div className="App">
        My App
 
-       <Projects />
+       <Projects projects={this.state.projects}/>
       </div>
     );
   }
